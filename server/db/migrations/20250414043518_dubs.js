46ffd7d3140e50a6aba9822f44622f20f0beceb5
Eilish McEwan <eilishmcewan@gmail.com>
2025-04-14 16:41:56 +1200

    first migration

---
+++ server/db/migrations/20250414043518_dubs.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('dubs', (table) => {
+   table.increments('id')
+   table.string('dub')
+   table.timestamps(true,true)
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('dubs')
+};
